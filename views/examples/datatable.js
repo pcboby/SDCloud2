64209c7010cb032f4813e61aaf97cbfb23834f20
pcboby <pcboby@126.com>
2018-04-24 23:32:45 +0800

    update

--- views/examples/datatable.js
+++ views/examples/datatable.js
@@ -1,38 +1,185 @@
 define([
-    "views/modules/editor",
-    "views/modules/topsales",
     "models/products"
-], function (editor, topsales, products) {
+], function (productsApi) {
+    // 新增行默认数据
+    var newRow = [];
+    // 表头定义
+    var columns = [{
+        id: "id",
+        header: "#",
+        width: 50
+    }, {
+        id: "opt",
+        header: "&nbsp;",
+        width: 70,
+        template: function (obj) {
+            var btns = "<span  style=' cursor:pointer;' class='webix_icon fa-pencil'></span>"
+            btns += "<span  style='cursor:pointer;' class='webix_icon fa-trash-o'></span>"
+            return btns
+        }
+    }, {
+        id: "code",
+        header: "Code",
+        sort: "string",
+        minWidth: 80,
+        fillspace: 1
+    }, {
+        id: "name",
+        header: "Name",
+        sort: "string",
+        minWidth: 120,
+        fillspace: 2,
+        editor: "text"
+    }, {
+        id: "categoryName",
+        header: "Category",
+        sort: "string",
+        minWidth: 120,
+        fillspace: 2,
+        template: "<div class='category#category#'>#categoryName#</div>"
+    }, {
+        id: "price",
+        header: "Price",
+        sort: "int",
+        minWidth: 80,
+        fillspace: 1,
+        format: webix.i18n.priceFormat
+    }, {
+        id: "quantity",
+        header: "Quantity",
+        sort: "int",
+        minWidth: 60,
+        fillspace: 1
+    }, {
+        id: "statusName",
+        header: "Status",
+        minWidth: 75,
+        sort: "string",
+        minWidth: 70,
+        fillspace: 1,
+        template: "<span class='status status#status#'>#statusName#</span>"
+    }];
+    // 工具栏
+    var toolbar = {
+        height: 40,
+        cols: [{
+                view: 'button',
+                type: 'iconButton',
+                icon: 'plus',
+                width: 80,
+                label: 'add',
+                click: function () {
+                    $$("datatable:datagrid").add(newRow, 0);
+                }
+            }, {
+                view: "button",
+                type: "iconButton",
+                icon: "file-excel-o",
+                width: 150,
+                label: "Export To Excel",
+                click: function () {
+                    $$("datatable:datagrid").exportToExcel();
+                }
+            },
+            {
+                view: "button",
+                type: "iconButton",
+                icon: "refresh",
+                width: 100,
+                label: "Refresh",
+                click: function () {
+                    var grid = $$("datatable:datagrid");
+                    grid.clearAll();
+                    grid.showProgress();
+                    webix.delay(function () {
+                        grid.parse(products.getAll);
+                        grid.hideProgress();
+                    }, null, null, 300);
 
+                }
+            },
+            {},
+            {
+                view: "richselect",
+                id: "order_filter",
+                value: "all",
+                maxWidth: 300,
+                minWidth: 250,
+                vertical: true,
+                labelWidth: 110,
+                options: [{
+                        id: "all",
+                        value: "All"
+                    },
+                    {
+                        id: "1",
+                        value: "Published"
+                    },
+                    {
+                        id: "2",
+                        value: "Not published"
+                    },
+                    {
+                        id: "0",
+                        value: "Deleted"
+                    }
+                ],
+                label: "Filter products",
+                on: {
+                    onChange: function () {
+                        var val = this.getValue();
+                        if (val == "all")
+                            $$("datatable:datagrid").filter("#status#", "");
+                        else
+                            $$("datatable:datagrid").filter("#status#", val);
+                    }
+                }
+            }
+        ]
+    };
 
-    var grid = {
-        id: "productsData",
-        view: "datatable", select: true, editable: true, editaction: "dblclick",
-        columns: [
-            { id: "id", header: "#", width: 50 },
-            { id: "code", header: ["Code", { content: "textFilter" }], sort: "string", minWidth: 80, fillspace: 1 },
-            { id: "name", header: ["Name", { content: "textFilter" }], sort: "string", minWidth: 120, fillspace: 2, editor: "text" },
-            { id: "categoryName", header: ["Category", { content: "selectFilter" }], sort: "string", minWidth: 120, fillspace: 2, editor: "select", template: "<div class='category#category#'>#categoryName#</div>" },
-            { id: "price", header: ["Price"], sort: "int", minWidth: 80, fillspace: 1, format: webix.i18n.priceFormat },
-            { id: "quantity", header: ["Quantity"], sort: "int", minWidth: 60, fillspace: 1 },
-            { id: "statusName", header: ["Status"], minWidth: 75, sort: "string", minWidth: 70, fillspace: 1, template: "<span class='status status#status#'>#statusName#</span>" },
+    // 翻页
+    var pagation = {
+        view: "toolbar",
+        css: "highlighted_header header6",
+        paddingX: 5,
+        paddingY: 5,
+        height: 40,
+        cols: [{
+                view: "pager",
+                id: "pagerA",
+                template: "{common.first()}{common.prev()}&nbsp; {common.pages()}&nbsp; {common.next()}{common.last()}",
+                autosize: true,
+                height: 35,
+                group: 5
+            }
 
-            { id: "edit", header: "&nbsp;", width: 35, template: "<span  style=' cursor:pointer;' class='webix_icon fa-pencil'></span>" },
-            { id: "delete", header: "&nbsp;", width: 35, template: "<span  style='cursor:pointer;' class='webix_icon fa-trash-o'></span>" }
-        ],
+        ]
+    }
+
+    // 列表
+    var datagrid = {
+        id: "datatable:datagrid",
+        view: "datatable",
+        // select: true,
+        editable: true,
+        editaction: "dblclick",
+        columns: columns,
         pager: "pagerA",
         "export": true,
-        data: products.getAll,
+        data: productsApi.getAll,
         onClick: {
             "fa-trash-o": function (e, id, node) {
                 webix.confirm({
-                    text: "The product will be deleted. <br/> Are you sure?", ok: "Yes", cancel: "Cancel",
+                    text: "The product will be deleted. <br/> Are you sure?",
+                    ok: "Yes",
+                    cancel: "Cancel",
                     callback: function (res) {
                         if (res) {
-                            var item = webix.$$("productsData").getItem(id);
+                            var item = webix.$$("datatable:datagrid").getItem(id);
                             item.status = "0";
                             item.statusName = "Deleted";
-                            webix.$$("productsData").refresh(id);
+                            webix.$$("datatable:datagrid").refresh(id);
                         }
                     }
                 });
@@ -43,66 +190,15 @@ define([
         }
     };
 
-
+    // 主结构
     var layout = {
         type: "space",
-
         rows: [
-            {
-                height: 40,
-
-                cols: [
-                    { view: "button", type: "iconButton", icon: "file-excel-o", width: 150, label: "Export To Excel", click: function () { $$("productsData").exportToExcel(); } },
-                    {
-                        view: "button", type: "iconButton", icon: "refresh", width: 100, label: "Refresh", click: function () {
-                            var grid = $$("productsData");
-                            grid.clearAll();
-                            grid.showProgress();
-                            webix.delay(function () {
-                                grid.parse(products.getAll);
-                                grid.hideProgress();
-                            }, null, null, 300);
-
-                        }
-                    },
-                    {},
-                    {
-                        view: "richselect", id: "order_filter", value: "all", maxWidth: 300, minWidth: 250, vertical: true, labelWidth: 110, options: [
-                            { id: "all", value: "All" },
-                            { id: "1", value: "Published" },
-                            { id: "2", value: "Not published" },
-                            { id: "0", value: "Deleted" }
-                        ], label: "Filter products", on: {
-                            onChange: function () {
-                                var val = this.getValue();
-                                if (val == "all")
-                                    $$("productsData").filter("#status#", "");
-                                else
-                                    $$("productsData").filter("#status#", val);
-                            }
-                        }
-                    }
-                ]
-            },
+            toolbar,
             {
                 rows: [
-                    grid,
-                    {
-                        view: "toolbar",
-                        css: "highlighted_header header6",
-                        paddingX: 5,
-                        paddingY: 5,
-                        height: 40,
-                        cols: [{
-                            view: "pager", id: "pagerA",
-                            template: "{common.first()}{common.prev()}&nbsp; {common.pages()}&nbsp; {common.next()}{common.last()}",
-                            autosize: true,
-                            height: 35,
-                            group: 5
-                        }
-
-                        ]
-                    }
+                    datagrid,
+                    pagation
                 ]
             }
 
@@ -111,6 +207,8 @@ define([
 
     };
 
-    return { $ui: layout };
+    return {
+        $ui: layout
+    };
 
 });
